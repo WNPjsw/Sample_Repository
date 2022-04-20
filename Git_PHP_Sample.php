fe1f2a95b261a6deb64219a083651b5d2a06e8a8
jsw <jsw@wnpsoft.co.kr>
2022-04-20 15:20:10 +0900

    Sample_db -> emp table print

--- Git_PHP_Sample.php
+++ Git_PHP_Sample.php
@@ -12,8 +12,19 @@ if (!$conn) {
 
 $sql = "select * from emp";
 
-if (mysqli_query($conn,$sql)) {
-    echo "쿼리 성공";
+if ($result=mysqli_query($conn,$sql)) {
+    echo "쿼리 성공\n";
+    $data=mysqli_fetch_array($result);
+    foreach($data as $value){
+        echo "EMPNO = > ".$data['EMPNO']." ";
+        echo "ENAME = > ".$data['ENAME']." ";
+        echo "JOB = > ".$data['JOB']." ";
+        echo "MGR = > ".$data['MGR']." ";
+        echo "HIREDATE = > ".$data['HIREDATE']." ";
+        echo "SAL = > ".$data['SAL']." ";
+        echo "COMM = > ".$data['COMM']." ";
+        echo "DPTNO = > ".$data['DPTNO']."\n";
+    }
 } else {
     echo ("오류 발생 : ".mysqli_error($conn));
 }
